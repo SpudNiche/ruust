91e04e66769a4af94ff39b538a218b7588e91097
nic <nic@iotato.me>
2021-01-04 15:53:01 -0500

    This code receives a wav file, decodes it into f32 samples, and re-encodes it into a new 'test.wav' file, which sounds exactly the same (so far as I can tell) for my example, but it's actually a significantly larger file, and I'm unsure why...

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@
 // Purpose: Messing around with audio-manipulation libraries in Rust
 
 // Test #1: "creak" library - dumps f32 samples of an audio file out to stdout
+// Test #2: "hound" library - create a wav file from f32 samples
 
 use std::{env};
 use creak;
+use hound;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Outout wav file spec
+    let spec = hound::WavSpec {
+        channels: 2,
+        sample_rate: 48000,
+        bits_per_sample: 32,
+        sample_format: hound::SampleFormat::Float,
+    };
+
     // Get a file name from the cmdline args
     let file_name = match env::args().nth(1) {
         Some(arg) => arg,
@@ -30,15 +40,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut num_samples: usize = 0;
 
-    // Dump all samples to stdout
+    // Dump all samples into a new audio file
+    let mut writer = hound::WavWriter::create("test.wav", spec).unwrap();
     for sample in decoder.into_samples()? {
-        let mut samp = &mut sample.unwrap();
-        if (*samp + 0.1) < 1.0 {
-            // Print as f32 values (-1.0 -> 1.0)
-            *samp += 0.1;
-            println!("{:?}", samp);
-        }
+        //let mut samp = &mut sample.unwrap();
+        //if (*samp + 0.1) < 1.0 {
+        //    // Print as f32 values (-1.0 -> 1.0)
+        //    *samp += 0.1;
+        //    println!("{:?}", samp);
+        //}
 
+        writer.write_sample(sample.unwrap()).unwrap();
         // Print as 4, byte chunks, litte-endian
         //println!("{:?}", &sample.unwrap().to_le_bytes());
 
