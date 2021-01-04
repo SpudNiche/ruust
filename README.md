f179328bfd2372c9989ea9df951a29441676bec2
nic <nic@iotato.me>
2021-01-04 14:22:09 -0500

    Basic printing of the samples (-1.0 to 1.0) values after passing a file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ruust
 Messing around with audio manipulation in Rust. Pronounced "roost"?
+
+## Options
+Starting out by taking a peek at this list of crates for all things audio: https://lib.rs/multimedia/audio
+
+I've never done any sort of audio file manipulation programmatically, so whatever ends up in this project will be quite simple. At the very least, I want to be able to change some aspect of an audio file (i.e. pitch).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+// Author: Nicholas LaJoie
+// Date: January 4, 2021
+// Purpose: Messing around with audio-manipulation libraries in Rust
+
+// Test #1: "creak" library - dumps f32 samples of an audio file out to stdout
+
+use std::{env, io, io::Write};
+use creak;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get a file name from the cmdline args
+    let file_name = match env::args().nth(1) {
+        Some(arg) => arg,
+        None => {
+            eprintln!("No audio file specified!");
+            return Ok(())
+        }
+    };
+
+    // Open an audio file of any supported format with one function call
+    let decoder = creak::Decoder::open(&file_name)?;
+
+    // Print basic audio info to stderr
+    let info = decoder.info();
+    eprintln!("Format: {}; Channels: {}; Sample Rate: {}Hz", 
+        info.format(), 
+        info.channels(), 
+        info.sample_rate()
+    );
+
+    let mut stdout = io::stdout();
+    let mut num_samples: usize = 0;
+
+    // Dump all samples to stdout
+    for sample in decoder.into_samples()? {
+        // stdout.write(&sample?.to_le_bytes())?;
+        //print!("{:?}", &sample?.to_le_bytes());
+        println!("{:?}", &sample.unwrap());
+        num_samples += 1;
+    }
+
+    eprintln!("{} samples(s) read.", num_samples);
+    Ok(())
 }
